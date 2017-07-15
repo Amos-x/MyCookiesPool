b3d4f27297f9bff5d1395ba6774aceaf5ce48480
Amos <379833553@qq.com>
2017-07-15 19:24:50 +0800

    add a tool.py

--- cookiespool/generator.py
+++ cookiespool/generator.py
@@ -1,26 +1,19 @@
 from cookiespool.settings import *
 from cookiespool.Redis import CookiesRedisclient,AccountRedisclient
-
+from cookiespool.tool import Captcha_By_Hand
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from PIL import Image
+
 
 class CookiesGenerator(object):
-    def __init__(self,name='default',browser_type=DEFAULT_BROWSER):
+    def __init__(self,name='default'):
         self.name = name
-        self.browser_type = browser_type
         self.cookies_db = CookiesRedisclient(name=self.name)
         self.account_db = AccountRedisclient(name=self.name)
-
-    def _init_browser(self):
-        """初始化浏览器"""
-        if self.browser_type == 'PhantomJS':
-            pass
-        elif self.browser_type =='Chrome':
-            self.browser = webdriver.Chrome()
-            self.wait = WebDriverWait(self.browser, 10)
 
     def get_new_cookis(self,username,password):
         raise NotImplementedError
@@ -38,22 +31,27 @@ class CookiesGenerator(object):
         accounts = list(accounts)
         exist_username = [x.get('username') for x in all_cookies]
         if len(accounts):
-            self._init_browser()
             for account in accounts:
                 if not account.get('username') in exist_username:
                     print('获取cookie:'+account.get('username'))
                     self.set_cookies(account)
-            self.browser.close()
-            del self.browser
         else:
             print('账号为空，不进行cookies生成')
 
 
 class WeiboCookiesGenerator(CookiesGenerator):
     def __init__(self,name='weibo',browser_type=DEFAULT_BROWSER):
-        CookiesGenerator.__init__(self, name, browser_type)
+        CookiesGenerator.__init__(self, name)
+        #初始化浏览器
+        self.browser_type = browser_type
+        if self.browser_type == 'PhantomJS':
+            pass
+        elif self.browser_type =='Chrome':
+            self.browser = webdriver.Chrome()
+        self.wait = WebDriverWait(self.browser, 10)
 
     def _login_isok(self,username):
+        """判断是否登录成功"""
         try:
             sign = self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'p.me_name')))
             if sign:
@@ -67,22 +65,34 @@ class WeiboCookiesGenerator(CookiesGenerator):
 
     def get_new_cookis(self,username,password):
         """cookie 获取函数"""
-        self.browser.delete_all_cookies()
-        self.browser.get('http://my.sina.com.cn/profile/unlogin')
-        login_button = self.wait.until(EC.element_to_be_clickable((By.ID, 'hd_login')))
-        login_button.click()
-        username_window = self.wait.until(EC.visibility_of_element_located((By.NAME, 'loginname')))
-        username_window.send_keys(username)
-        password_window = self.wait.until(EC.visibility_of_element_located((By.NAME, 'password')))
-        password_window.send_keys(password)
-        button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.login_btn')))
-        button.click()
-        result = self._login_isok(username)
-        if result:
-            return result
-        # 验证码处理这里先空着，下次再完善
-        print('需要验证码')
-        return
+        try:
+            self.browser.delete_all_cookies()
+            self.browser.get('http://my.sina.com.cn/profile/unlogin')
+            login_button = self.wait.until(EC.element_to_be_clickable((By.ID, 'hd_login')))
+            login_button.click()
+            username_window = self.wait.until(EC.visibility_of_element_located((By.NAME, 'loginname')))
+            username_window.send_keys(username)
+            password_window = self.wait.until(EC.visibility_of_element_located((By.NAME, 'password')))
+            password_window.send_keys(password)
+            button = self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.login_btn')))
+            button.click()
+            result = self._login_isok(username)
+            if result:
+                return result
+            # 加了一个手动识别验证码的弹窗，只由于少量cookies测试时。
+            print('需要验证码')
+            captcha_img = self.wait(EC.visibility_of_element_located((By.CSS_SELECTOR, 'img.yzm')))
+            self.browser.save_screenshot('yzm.png')
+            img = Image.open('yzm.png')
+            img.show()
+            s = Captcha_By_Hand()
+            s.run(img=img)
+            print(s.captcha)
+        except:
+            print('登录错误，可能次数过多，被限制登录')
 
 
 
+
+if __name__ == '__main__':
+    pass
---
+++ cookiespool/tool.py
@@ -0,0 +1,27 @@
+from PIL import Image,ImageTk
+import tkinter as tk
+
+class Captcha_By_Hand(object):
+    """一个手动输入验证码的弹窗，用与少量登录时"""
+    def callback(self):
+        self.captcha = self.text.get()
+        self.input_window.destroy()
+        self.input_window.quit()
+
+    def run(self,img):
+        self.input_window = tk.Tk()
+        self.input_window.title('需要验证码')
+        self.input_window.geometry('250x130')
+        self.photo = ImageTk.PhotoImage(img)
+        tk.Label(self.input_window, image=self.photo).pack()
+        tk.Label(self.input_window, text='请输入验证码：').pack()
+        self.text = tk.StringVar()
+        tk.Entry(self.input_window, textvariable=self.text).pack()
+        tk.Button(self.input_window, text='确定', command=self.callback).pack(ipadx=10, ipady=5)
+        self.input_window.mainloop()
+
+
+if __name__ == '__main__':
+    s = Captcha_By_Hand()
+    s.run(Image.open('C:\\Users\Administrator\Desktop\captcha.jpg'))
+    print(s.captcha)
